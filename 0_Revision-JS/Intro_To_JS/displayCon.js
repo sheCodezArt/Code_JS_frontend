5286c266e98122aabb62ede1f059bf9bca56a8b9
Jennie547 <jessica4edigin@gmail.com>
2025-03-03 16:01:38 +0100

    take care

---
+++ 0_Revision-JS/Intro_To_JS/displayCon.js
@@ -0,0 +1,10 @@
+function conDis() {
+
+  let par = document.createElement('p');
+  // if (par) {
+    let msg = "Check the console!";
+    par.textContent = msg;
+  // }
+  console.log("Did you know I was here?!");
+  
+}
